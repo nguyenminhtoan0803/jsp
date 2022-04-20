ae7450d47b8126f4acce27f49677be85041791a4
= <=>
2022-04-20 14:41:07 +0700

    jsp_servlet_jdbc

---
+++ src/main/java/com/laptrinhjavaweb/utils/FormUtil.java
@@ -0,0 +1,24 @@
+package com.laptrinhjavaweb.utils;
+
+import java.lang.reflect.InvocationTargetException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.beanutils.BeanUtils;
+
+public class FormUtil {
+
+	@SuppressWarnings("unchecked")
+	public static <T> T toModel(Class<T> tClass, HttpServletRequest request) {
+		T object = null;
+		try {
+			object = tClass.getDeclaredConstructor().newInstance();
+			BeanUtils.populate(object, request.getParameterMap());
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+				| NoSuchMethodException | SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return object;
+	}
+}
